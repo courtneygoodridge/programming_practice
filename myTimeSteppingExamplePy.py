c5fc55183ea050bcf512d6cee8b98943ecd9b785
callummole <c.d.mole@leeds.ac.uk>
2019-02-26 10:55:58 +0000

    cdm comments

--- myTimeSteppingExamplePy.py
+++ myTimeSteppingExamplePy.py
@@ -20,8 +20,11 @@ Y = [0] * TotalSamples # y the rows length of number of samples
 
 #### Update loop
 
-for i in TotalSamples:
-    if i > 1:
+#this fails because TotalSamples is an integer, not a list, so is not iterable (python cannot loop through it)
+for i in TotalSamples: 
+    if i > 1: #unlike matlab and R, python is zero-indexed (the first index in an array is 0)
+
+        # cos() and sin() are not functions in the python base language. Check out the numpy package, you'll be using that a lot.
         X[i] = X[i-1] + cos(heading_angle) * speed * timestep # the cosine of the heading angle mulitpled by speed multiplied by the timestep
         Y[i] = Y[i-1] + sin(heading_angle) * speed * timestep
             
